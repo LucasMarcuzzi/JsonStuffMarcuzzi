cbb9aa1e28d09644aaea47d43b66024b5e0af1e6
LucasMarcuzzi <lucasmarcuzzi@hotmail.com>
2017-04-25 12:00:14 -0300

    Inital Commit
    Se suben los projectos terminados. Posiblemente el unico commit.

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.IO;
+
+using Newtonsoft.Json.Linq;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el titulo de la pelicula:");
+
+            string pelicula = Convert.ToString(Console.ReadLine());
+
+            WebRequest req = WebRequest.Create("http://www.omdbapi.com/?t="+pelicula);
+
+            WebResponse respuesta = req.GetResponse();
+
+            Stream stream = respuesta.GetResponseStream();
+
+            StreamReader sr = new StreamReader(stream);
+
+            JObject data = JObject.Parse(sr.ReadToEnd());
+
+            string titulo = (string)data["Year"];
+
+            Console.WriteLine("El año de salida de esa pelicula es:");
+
+            Console.WriteLine(titulo);
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using System.Net;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Ingrese el Nombre de la Ciudad:");
+
+            string ciudad = Convert.ToString(Console.ReadLine());
+
+            WebRequest req = WebRequest.Create("https://maps.googleapis.com/maps/api/geocode/json?address=" + ciudad +"&key=AIzaSyDgwOpkxGZpV1tYSmh6L3ZEflt6WfnGeCw");
+
+            WebResponse respuesta = req.GetResponse();
+
+            Stream stream = respuesta.GetResponseStream();
+
+            StreamReader sr = new StreamReader(stream);
+
+            JObject data = JObject.Parse(sr.ReadToEnd());
+
+            string address = (string)data["results"][0]["address_components"][2]["long_name"];
+
+            Console.WriteLine("Esa ciudad se encuentra en el pais:");
+
+            Console.WriteLine(address);
+
+            Console.ReadKey();
+        }
+    }
+}
